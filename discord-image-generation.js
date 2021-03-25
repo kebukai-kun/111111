1d4bdf9a8c875e06a127812ea9d272acd0746361
kebukai-kun <78577945+kebukai-kun@users.noreply.github.com>
2021-03-25 17:21:46 -0300

    Add files via upload

---
+++ discord-image-generation.js
@@ -0,0 +1,28 @@
+// Import the discord.js library.
+const Discord = require("discord.js")
+// Create a new discord.js client.
+const bot = new Discord.Client()
+
+const DIG = require("discord-image-generation");
+> You can also destructure to avoid repeating DIG.
+
+// Listen to the ready event
+bot.on("ready", () => {
+    console.log("ok");  
+})
+
+// Listen to the message event
+bot.on("message", async (message) => {
+    if (message.content === "*delete") {
+        // Get the avatarUrl of the user
+        let avatar = message.author.displayAvatarURL({ dynamic: false, format: 'png' });
+        // Make the image
+        let img = await new DIG.Delete().getImage(avatar)
+        // Add the image as an attachement
+        let attach = new Discord.MessageAttachment(img, "delete.png");;
+        message.channel.send(attach)
+    }
+})
+
+// Log in to the bot
+bot.login("super_secret_token")
